4da6cde6f1fa6c6067dd3685d2522084ff634217
SAURABHSHINDE1 <saurabhshinde779845@gmail.com>
2026-04-06 13:13:11 +0530

    6 April 2026

--- server.js
+++ server.js
@@ -1,42 +1,83 @@
 import express from 'express'
 import mysql from 'mysql2'
+import dotenv from 'dotenv'
+dotenv.config()
 
 const app = express()
-
-const port = 4000
+const port = process.env.PORT
 
 const db = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"system",
-    database:"test"
+        host:process.env.HOST,
+        user:process.env.USER,
+        password:process.env.PASSWORD,
+        database:process.env.DATABASE
 })
 
-db.connect(()=>{
-    console.log("Database is connected successfully !")
+db.connect((error)=>{
+
+    if(error){
+        console.log("some error in database connection:-" , error)
+    }
+    else{
+        console.log("Database is connected successfully ! ")
+    }
+
 })
 
-app.get('/' , (req, res)=>{
 
-    res.send("hello")
+app.get('/' , (req , res)=>{
+
+    res.send("hello from my api")
 
 })
 
+app.get('/student' , (req , res)=>{
 
-app.get('/user' , (req , res)=>{
+    const sql = "select * from student"
 
-    db.query('select * from user' , (error ,result)=>{
-
+    db.query(sql ,(error , result)=>{
         if(error){
-            res.send("some err" + error)
+            res.send("some err " + error)
         }
 
         res.send(result)
-
     })
 
 })
 
+
+
+app.get('/student/male' , (req , res)=>{
+
+    const sql = "select * from student where gender = 'male' "
+
+    db.query(sql ,(error , result)=>{
+        if(error){
+            res.send("some err " + error)
+        }
+
+        res.send(result)
+    })
+
+})
+
+app.get('/student/id' , (req , res)=>{
+
+    const sql = "select * from student where id = 5 "
+
+    db.query(sql ,(error , result)=>{
+        if(error){
+            res.send("some err " + error)
+        }
+
+        res.send(result)
+    })
+
+})
+
+
+
+
 app.listen(port , ()=>{
-    console.log(`Server is Running http://localhost:${port}`)
+    console.log(`The server is Running on ${port}`)
 })
