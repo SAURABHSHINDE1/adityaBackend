cc504a90e8db4954d5b9c91ea033d20fc0209167
SAURABHSHINDE1 <saurabhshinde779845@gmail.com>
2026-04-07 12:26:02 +0530

    7 April 2026

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 import express from 'express'
 import mysql from 'mysql2'
 import dotenv from 'dotenv'
+
 dotenv.config()
 
 const app = express()
+
+app.use(express.json())
+
 const port = process.env.PORT
 
 const db = mysql.createConnection({
@@ -31,49 +35,107 @@ app.get('/' , (req , res)=>{
 
 })
 
-app.get('/student' , (req , res)=>{
+// app.get('/student' , (req , res)=>{
 
-    const sql = "select * from student"
+//     const sql = "select * from student"
 
-    db.query(sql ,(error , result)=>{
-        if(error){
-            res.send("some err " + error)
-        }
+//     db.query(sql ,(error , result)=>{
+//         if(error){
+//             res.send("some err " + error)
+//         }
 
-        res.send(result)
+//         res.send(result)
+//     })
+
+// })
+
+// app.get('/student/male' , (req , res)=>{
+
+//     const sql = "select * from student where gender = 'male' "
+
+//     db.query(sql ,(error , result)=>{
+//         if(error){
+//             res.send("some err " + error)
+//         }
+//         res.send(result)
+//     })
+// })
+
+// app.get('/student/id' , (req , res)=>{
+
+//     const sql = "select * from student where id = 5 "
+
+//     db.query(sql ,(error , result)=>{
+//         if(error){
+//             res.send("some err " + error)
+//         }
+
+//         res.send(result)
+//     })
+
+// })
+
+// app.get('/student' , (req , res)=>{
+
+//     const id = req.query.id
+//     const city = req.query.city
+
+//     const sql = 'select * from student where id = ? and address = ?'
+
+//     db.query(sql, [id , city], (error , result)=>{
+
+//         if(error){
+//             res.send("some error in query" , error)
+//         }
+
+//         res.send(result)
+
+//     })
+
+// })
+
+
+
+// app.get('/student/:id/:city' , (req , res)=>{
+
+//     // const id = req.params.id
+//     // const city = req.params.city
+
+//     const {id , city} = req.params
+
+//     const sql = 'select * from student where id = ? and address = ?'
+
+//     db.query(sql, [id , city], (error , result)=>{
+
+//         if(error){
+//             res.send("some error in query" , error)
+//         }
+
+//         res.send(result)
+
+//     })
+
+// })
+
+
+app.post('/add/student' , (req , res)=>{
+
+    const { name , email, age, address, gender} = req.body
+
+    const sql = 'insert into student(name , email, age, address, gender) values (?,?,?,?,?)'
+
+    db.query(sql , [name , email, age, address, gender] , (error , result )=>{
+            if(error){
+                return res.json({message:"some error"})
+            }
+            else{
+                return res.json({message :"user added successfully"})
+            }
+
     })
 
 })
 
-
-
-app.get('/student/male' , (req , res)=>{
-
-    const sql = "select * from student where gender = 'male' "
-
-    db.query(sql ,(error , result)=>{
-        if(error){
-            res.send("some err " + error)
-        }
-
-        res.send(result)
-    })
-
-})
-
-app.get('/student/id' , (req , res)=>{
-
-    const sql = "select * from student where id = 5 "
-
-    db.query(sql ,(error , result)=>{
-        if(error){
-            res.send("some err " + error)
-        }
-
-        res.send(result)
-    })
-
-})
 
 
 
